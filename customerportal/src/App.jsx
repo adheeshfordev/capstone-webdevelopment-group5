381632a51ada1ba8b76639ab54ab2422c74b042d
Bikramjot Singh Dhindsa <bikramjot.singh@iitgn.ac.in>
2024-05-28 21:44:01 -0400

    React setup using Vite, components, router and pages

---
+++ customerportal/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+
+function App() {
+
+  return (
+    <>
+     <BrowserRouter> 
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App ;
---
+++ customerportal/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header.jsx'
+import Footer from '../components/Footer.jsx'
+
+export default function Home() {
+  return (
+    <div>
+        <Header />
+        Home
+
+        <Footer />
+    </div>
+  )
+}
