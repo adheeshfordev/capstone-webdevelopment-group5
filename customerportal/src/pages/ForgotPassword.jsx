9a775bda0191ac1286273c0b1d2e933b3c26b2ab
Bikramjot Singh Dhindsa <bikramjot.singh@iitgn.ac.in>
2024-05-30 18:56:48 -0400

    signup/signin/forgotpassword

---
+++ customerportal/src/pages/ForgotPassword.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export default function ForgotPassword() {
+    const [error, setError] = useState();
+    const [success, setSuccess] = useState();
+
+  return (
+    <>  
+        <div className="container">
+            <h1 className="title">
+                <span className="text-muted">Forgot-</span>
+                <span className="text-dark">Password</span>
+            </h1>
+            <form className="form">
+                <p className="text-dark-forgot-password">
+                    Please enter your associated email here:
+                </p>
+                <input type="text" placeholder="email@email.com" id="email" className="input" />
+                <div className="button-container">
+                    <button className="button">Send Email</button>
+                </div>
+            </form>
+            {error && <p className='error-message'>{error}</p>}
+            {success && <p className='.success-message'>{success}</p>}
+        </div>
+    </>
+  )
+}
