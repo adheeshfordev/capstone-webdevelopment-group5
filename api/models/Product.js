729802479e3c61833b6fd89390558bf1e6394f4e
Amal Baiju <amalbaiju21@gmail.com>
2024-05-31 12:50:58 -0400

    Updated Product model

--- api/models/Product.js
+++ api/models/Product.js
@@ -3,7 +3,11 @@ const productSchema = new mongoose.Schema({
     description: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
+    platform: { type: String, required: true }, 
     imageUrl: { type: String, required: true },
+    developer: { type: String, required: true }, 
+    publisher: { type: String, required: true }, 
+    releaseDate: { type: Date, required: true }, 
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
   });
