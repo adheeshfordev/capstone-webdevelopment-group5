ab7a862ea7d9a823b9c829fabfb0394c0e2d6f9d
Adheesh L P <adheesh.lp@teronext.com>
2024-06-13 13:12:39 -0400

    Userlist added

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,1 +1,13 @@
 const user = require('../models/User')
+
+const userList = async (req, res) => {
+    try {
+        const users = await user.find({});
+        res.json({ users });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+module.exports = { productList }
