d6788b2c3039cd54c043b8e90bb3a2f6a2005439
Adheesh L P <adheesh.lp@teronext.com>
2024-06-13 16:40:36 -0400

    better code

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,7 +2,7 @@ const user = require('../models/User')
 
 const userList = async (req, res) => {
     try {
-        const users = await user.find({}, {password:0});
+        const users = await user.find({}, {password:0, _id:0});
         res.json({ users });
     } catch (error) {
         console.log(error);
