5ca83a4b9c14d9d48f71264490613f25f9fb0233
Suresh thagunna <suresh.thagunna77@gmail.com>
2024-06-14 02:22:37 -0400

    enable cors for amdin; include header for pagination; duplicate _id as id

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -1,13 +1,23 @@
-const Product = require('../models/Product')
+const Product = require("../models/Product");
 
 const productList = async (req, res) => {
-    try {
-        const products = await Product.find({}, {_id: 0});
-        res.json({ products });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-}
+	try {
+		const products = await Product.find({});
+		// react-admin expects the id to be present for each entry(temporary solution)
+		const productsWithId = products.map((product) => {
+			return { id: product._id, ...product._doc };
+		});
+		// content-range header is expected by react-admin
+		res.header(
+			"Content-Range",
+			`products 0-${productsWithId.length - 1}/${productsWithId.length}`,
+		);
+		res.json(productsWithId);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
 
-module.exports = { productList }
+module.exports = { productList };
+
