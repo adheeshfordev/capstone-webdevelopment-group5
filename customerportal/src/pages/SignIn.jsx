966be7dcc3b175c87823aec177a70e7471ee152a
Bikramjot Singh Dhindsa <bikramjot.singh@iitgn.ac.in>
2024-06-14 17:11:19 -0400

    Intial draft of working API integration for signin and signup

--- customerportal/src/pages/SignIn.jsx
+++ customerportal/src/pages/SignIn.jsx
@@ -1,10 +1,50 @@
-import React, { useState } from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import Header from '../components/Header.jsx'
 
 export default function SignIn() {
 
-    const [error, setError] = useState();
-    const [success, setSuccess] = useState();
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
+
+    const [signinData, setSigninData] = useState(null);
+
+    const onChangeData =(event)=>{
+        setSigninData({
+            ...signinData,
+            [event.target.id]: event.target.value
+    })
+    
+    }
+
+    const submitForm = async (evt) => {
+        evt.preventDefault();
+        
+        try {
+            console.log(signinData);
+            const response = await fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(signinData)
+                    });
+
+            const data = await response.json();
+            console.log(data);
+            setError(data.error);
+            setSuccess(data.token);
+        }catch(error){
+            setSuccess(null);
+            console.log(error);
+            setError(error);
+        }
+
+    }
+
+//    console.log(signinData);
+//    console.log(success);
+//    console.log(error);
+
   return (
     <> 
     <Header />
@@ -13,9 +53,9 @@ export default function SignIn() {
                 <span className="text-muted">Sign-</span>
                 <span className="text-dark">In</span>
             </h1>
-            <form className="form">
-                <input type="text" placeholder="username" id="username" className="input" />
-                <input type="text" placeholder="password" id="password" className="input" />
+            <form className="form" onSubmit={submitForm}>
+                <input type="text" placeholder="email" id="email" className="input" onChange={onChangeData}/>
+                <input type="text" placeholder="password" id="password" className="input" onChange={onChangeData}/>
                 <div className="button-container">
                     <button className="button">Sign-In</button>
                 </div>
