639163f40062476584eaf6337321c1a389ca5ce0
Suresh thagunna <suresh.thagunna77@gmail.com>
2024-07-11 17:29:58 -0400

    admin(feat): product edit and product add

--- adminportal/src/App.jsx
+++ adminportal/src/App.jsx
@@ -1,14 +1,33 @@
 import "./App.css";
 
 import { Admin, Resource } from "react-admin";
-import { ProductIcon, ProductList } from "./components/ProductList";
+import { UserCreate } from "./components/UserCreate";
+import { UserIcon, UserList } from "./components/UserList";
+import {
+	ProductCreate,
+	ProductEdit,
+	ProductIcon,
+	ProductList,
+} from "./components/products";
 import wrappedRestProvider from "./wrapped-ra-data-simple-rest";
 
 function App() {
 	return (
 		<>
 			<Admin dataProvider={wrappedRestProvider}>
-				<Resource name="products" list={ProductList} icon={ProductIcon} />
+				<Resource
+					name="products"
+					list={ProductList}
+					edit={ProductEdit}
+					create={ProductCreate}
+					icon={ProductIcon}
+				/>
+				<Resource
+					name="users"
+					list={UserList}
+					create={UserCreate}
+					icon={UserIcon}
+				/>
 			</Admin>
 			,
 		</>
