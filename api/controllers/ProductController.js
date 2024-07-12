01ea740eed6089c8c027301b39d24b907e724bd5
Suresh thagunna <suresh.thagunna77@gmail.com>
2024-07-11 20:09:53 -0400

    api(feat): add controller to get single product

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -15,6 +15,20 @@ const productList = async (req, res) => {
 	}
 };
 
+// Get a single product
+const getProduct = async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		if (!product) {
+			return res.status(404).json({ error: "Product not found" });
+		}
+		res.json(product);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
+};
+
 // Create a new product
 const createProduct = async (req, res) => {
 	try {
@@ -58,4 +72,10 @@ const deleteProduct = async (req, res) => {
 	}
 };
 
-module.exports = { productList, createProduct, updateProduct, deleteProduct };
+module.exports = {
+	productList,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+	getProduct,
+};
