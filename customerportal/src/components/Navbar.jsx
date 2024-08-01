779825152f3cdde0bbdaa95c7c46e4791d82ac26
Adheesh L P <adheesh.lp@teronext.com>
2024-08-01 16:29:28 -0400

    Profile link exposed

--- customerportal/src/components/Navbar.jsx
+++ customerportal/src/components/Navbar.jsx
@@ -62,7 +62,7 @@ class Navbar extends Component {
 					</ul>
 
 					<ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-						<li><a className="nav-link" href="#"><img src="/src/images/user.svg" /></a></li>
+						<li><a className="nav-link" href="/profile"><img src="/src/images/user.svg" /></a></li>
 						<li><a className="nav-link" href="/cart"><img src="/src/images/cart.svg" /></a></li>
 					</ul>
 				</div> 
