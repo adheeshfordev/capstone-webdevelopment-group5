5032d51143b73aa4cd5c24081845ed2ef7692bed
Adheesh L P <adheesh.lp@teronext.com>
2024-08-02 08:42:50 -0400

    Homepage logo fix

--- customerportal/src/components/Navbar.jsx
+++ customerportal/src/components/Navbar.jsx
@@ -50,7 +50,7 @@ class Navbar extends Component {
             <nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
 			<div className="container">
-				<a className="navbar-brand" href="index.html">Pixel Paradise<span>.</span></a>
+				<a className="navbar-brand" href="/">Pixel Paradise<span>.</span></a>
 
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
