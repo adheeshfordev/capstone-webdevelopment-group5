377f38b8588530594642848c872efdf460ed75a8
Adheesh L P <adheesh.lp@teronext.com>
2024-08-12 16:20:43 -0400

    firebase changes

--- api/utils/firebase.js
+++ api/utils/firebase.js
@@ -1,7 +1,9 @@
 const firebaseAdmin = require("firebase-admin");
 
-var serviceAccount = require("../service-account.json");
-
+//var serviceAccount = require("../service-account.json");
+const b64ServiceAccount = process.env.SERVICE_ACCOUNT_CODE;
+const serviceAccount = JSON.parse(Buffer.from(b64ServiceAccount, 'base64').toString('utf8'));
+console.log(serviceAccount);
 firebaseAdmin.initializeApp({
 	credential: firebaseAdmin.credential.cert(serviceAccount),
 	storageBucket: "pixelparadisecapstone.appspot.com"
