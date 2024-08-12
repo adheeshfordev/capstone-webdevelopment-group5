86ae6513c9c6d555a9a757d288a23f01915dbcb9
Adheesh L P <adheesh.lp@teronext.com>
2024-08-12 16:34:38 -0400

    fix that error

--- api/utils/firebase.js
+++ api/utils/firebase.js
@@ -2,7 +2,7 @@ const firebaseAdmin = require("firebase-admin");
 
 //var serviceAccount = require("../service-account.json");
 const b64ServiceAccount = process.env.SERVICE_ACCOUNT_CODE;
-const serviceAccount = JSON.parse(Buffer.from(b64ServiceAccount, 'base64').toString('utf8'));
+const serviceAccount = Buffer.from(b64ServiceAccount, 'base64').toString('utf8');
 console.log(serviceAccount);
 firebaseAdmin.initializeApp({
 	credential: firebaseAdmin.credential.cert(serviceAccount),
