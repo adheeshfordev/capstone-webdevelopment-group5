56c29c9c39247aab25556dc6464f2bf80ee59798
Adheesh L P <adheesh.lp@teronext.com>
2024-08-13 15:20:08 -0400

    cart model improved

--- api/models/Cart.js
+++ api/models/Cart.js
@@ -5,7 +5,8 @@ const cartSchema = new mongoose.Schema({
       {
         product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
         quantity: { type: Number, required: true, default: 1 },
-        price: { type: Number, required: true }
+        price: { type: Number, required: true },
+        imageUrl: {type: String, default: ""}
       }
     ],
     createdAt: { type: Date, default: Date.now },
